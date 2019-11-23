b0770964eeb3b3fcb0fa4472aa1decef7f038489
eva elvina <eva.elvina@koding.sch.id>
2019-11-23 13:32:07 +0700

    first commit

--- src/component/ButtonNavigation/component.js
+++ src/component/ButtonNavigation/component.js
@@ -4,6 +4,8 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Home';
 import FavoriteIcon from '@material-ui/icons/History';
 import LocationOnIcon from '@material-ui/icons/Person';
+import { Link } from 'react-router-dom';
+
 
 class Component extends React.Component {
     render() {
@@ -11,9 +13,9 @@ class Component extends React.Component {
         return(
             <div>
                 <BottomNavigation className={classes.btmNavi} style={{marginTop:180}}>
-                <BottomNavigationAction label="Home" icon={<RestoreIcon />} />
+                <Link to="/"><BottomNavigationAction label="Home" icon={<RestoreIcon />} /></Link>
                 <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-                <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                <Link to="/login"><BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} /></Link>
                 </BottomNavigation>
             </div>
         )
